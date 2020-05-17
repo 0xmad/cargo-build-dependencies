928a6ff9bcd33526a91b3fe7154f7364ee855dae
errmac-v <tmbrlkV@users.noreply.github.com>
2020-05-17 15:23:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# cargo-build-deps
+
+[![Crates.io](https://img.shields.io/crates/v/cargo-build-dependencies.svg)](https://crates.io/crates/cargo-build-dependencies)
+
+This tool extends [Cargo](https://doc.rust-lang.org/cargo/) to allow you to
+build only the dependencies in a given rust project. This is useful for docker
+builds where each build step is cached. The time it takes to build dependencies
+is often a significant portion of the overall build time. Therefore it is
+beneficial in docker builds to build dependencies in a separate step earlier
+than the main build. Since the dependency building step will be cached,
+dependencies will not need to be rebuilt when the project's own source code
+changes.
+
+Based on https://github.com/nacardin/cargo-build-deps
+
+
+## Install
+`cargo install cargo-build-dependencies`
+
+## Usage
+`cargo build-dependencies`
+
+## Example
+
+Change Dockerfile from
+
+```
+FROM rust:1.43 as rust-builder
+RUN mkdir /tmp/PROJECT_NAME
+WORKDIR /tmp/PROJECT_NAME
+COPY . .
+RUN cargo build  --release
+```
+
+to
+
+```
+FROM rust:1.43 as rust-builder
+RUN cargo install cargo-build-dependencies
+RUN cd /tmp && USER=root cargo new --bin PROJECT_NAME
+WORKDIR /tmp/PROJECT_NAME
+COPY Cargo.toml Cargo.lock ./
+RUN cargo build-dependencies --release
+COPY src /tmp/PROJECT_NAME/src
+RUN cargo build  --release
+```
---
+++ src/main.rs
@@ -0,0 +1,104 @@
+use clap::{App, Arg};
+use toml::Value as Toml;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
+
+fn main() {
+    let matched_args = App::new("cargo build-deps")
+        .arg(Arg::with_name("build-deps"))
+        .arg(Arg::with_name("release").long("release"))
+        .arg(Arg::with_name("target").long("target"))
+        .get_matches();
+
+    let is_release = matched_args.is_present("release");
+    let target = match matched_args.value_of("target") {
+        Some(value) => value,
+        None => "",
+    };
+
+    execute_command(Command::new("cargo").arg("update"));
+
+    let cargo_toml = get_toml("Cargo.toml");
+    let dependencies = parse_dependencies(&cargo_toml);
+
+    println!("Start building packages");
+
+    for dependency in dependencies {
+        build_package(&dependency, is_release, &target);
+    }
+
+    println!("Finished");
+}
+
+fn get_toml(file_path: &str) -> Toml {
+    let mut toml_file = File::open(file_path).expect(&format!("{} is not available", file_path));
+    let mut toml_string = String::new();
+    toml_file
+        .read_to_string(&mut toml_string)
+        .expect("Can't read file");
+    toml_string.parse().expect("Failed to parse toml")
+}
+
+fn parse_dependencies<'a>(toml: &'a Toml) -> Vec<String> {
+    match toml.get("dependencies") {
+        Some(&Toml::Table(ref pkgs)) => pkgs
+            .iter()
+            .map(|(name, value)| format_package(name, value))
+            .collect(),
+        _ => panic!("Failed to find dependencies in Cargo.toml"),
+    }
+}
+
+fn format_package(name: &String, value: &Toml) -> String {
+    match value {
+        Toml::String(string) => format!("{}:{}", name, string.replace("\"", "")),
+        Toml::Table(table) => {
+            let value = match table.get("version") {
+                Some(v) => v.to_string().replace("\"", ""),
+                None => "".to_string(),
+            };
+            format!("{}:{}", name, value)
+        }
+        _ => panic!("Failed to format package-id"),
+    }
+}
+
+fn build_package(pkg_name: &str, is_release: bool, target: &str) {
+    println!("Building package: {:?}", pkg_name);
+
+    let mut command = Command::new("cargo");
+    let command_with_args = command.arg("build").arg("-p").arg(pkg_name);
+
+    let command_with_args = if is_release {
+        command_with_args.arg("--release")
+    } else {
+        command_with_args
+    };
+
+    let command_with_args = if !target.is_empty() {
+        command_with_args.arg("--target=".to_owned() + target)
+    } else {
+        command_with_args
+    };
+
+    execute_command(command_with_args);
+}
+
+fn execute_command(command: &mut Command) {
+    let mut child = command
+        .envs(env::vars())
+        .spawn()
+        .expect("Failed to execute process");
+
+    let exit_status = child.wait().expect("Failed to run command");
+
+    if !exit_status.success() {
+        match exit_status.code() {
+            Some(code) => panic!("Exited with status code: {}", code),
+            None => panic!("Process terminated by signal"),
+        }
+    }
+}
